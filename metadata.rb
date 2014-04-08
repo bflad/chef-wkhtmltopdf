c0b92da459a38b6bdd507f09142b142790b7196f
Brian Flad <bflad417@gmail.com>
2014-04-07 20:52:43 -0400

    Rubocop automatic fixes

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
-name             'wkhtmltopdf'
-maintainer       'Brian Flad'
+name 'wkhtmltopdf'
+maintainer 'Brian Flad'
 maintainer_email 'bflad417@gmail.com'
-license          'Apache 2.0'
-description      'Installs wkhtmltoimage and wkhtmltopdf'
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+license 'Apache 2.0'
+description 'Installs wkhtmltoimage and wkhtmltopdf'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.2.0'
 
 recipe 'wkhtmltopdf', 'Installs wkhtmltoimage and wkhtmltopdf'
 recipe 'wkhtmltopdf::binary', 'Installs wkhtmltoimage and wkhtmltopdf binaries'
