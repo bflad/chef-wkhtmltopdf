c39747981ef275e9cf3499c652ce8ab34ed54b21
Ian Ehlert <ehlertij@gmail.com>
2014-08-26 11:25:16 -0500

    Use package instead

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,29 +1,4 @@
 default['wkhtmltopdf']['version']     = '0.12.1'
-default['wkhtmltopdf']['install_dir'] = '/usr/local/bin'
-default['wkhtmltopdf']['lib_dir']     = ''
 
-case node['platform_family']
-when 'mac_os_x', 'mac_os_x_server'
-  default['wkhtmltopdf']['dependency_packages'] = []
-  default['wkhtmltopdf']['platform'] = 'macosx-10.9.1-x86_64'
-when 'windows'
-  default['wkhtmltopdf']['dependency_packages'] = []
-  if node['kernel']['machine'] == 'x86_64'
-    default['wkhtmltopdf']['platform'] = 'win64'
-  else
-    default['wkhtmltopdf']['platform'] = 'win32'
-  end
-else
-  default['wkhtmltopdf']['dependency_packages'] = value_for_platform_family(
-    %w(debian) => %w(libfontconfig1 libssl0.9.8 libxext6 libxrender1),
-    %w(fedora rhel) => %w(fontconfig libXext libXrender openssl-devel urw-fonts)
-  )
-  if node['kernel']['machine'] == 'x86_64'
-    default['wkhtmltopdf']['platform'] = 'linux-amd64'
-  else
-    default['wkhtmltopdf']['platform'] = 'linux-i386'
-  end
-end
-
-default['wkhtmltopdf']['archive']     = "wkhtmltox-#{node['wkhtmltopdf']['version']}.tar.bz2"
-default['wkhtmltopdf']['mirror_url']  = "http://downloads.sourceforge.net/project/wkhtmltopdf/#{node['wkhtmltopdf']['version']}/wkhtmltox-#{node['wkhtmltopdf']['version']}.tar.bz2"
+default['wkhtmltopdf']['package']     = "wkhtmltox-#{node['wkhtmltopdf']['version']}_linux-precise-amd64.deb.deb"
+default['wkhtmltopdf']['mirror_url']  = "http://downloads.sourceforge.net/project/wkhtmltopdf/#{node['wkhtmltopdf']['version']}/#{node['wkhtmltopdf']['package']}"
--- recipes/binary.rb
+++ recipes/binary.rb
@@ -1,5 +1,5 @@
 cache_dir = Chef::Config[:file_cache_path]
-download_dest = File.join(cache_dir, node['wkhtmltopdf']['archive'])
+download_dest = File.join(cache_dir, node['wkhtmltopdf']['package'])
 wkhtmltopdf_version = Chef::Version.new(node['wkhtmltopdf']['version'])
 
 remote_file download_dest do
@@ -8,36 +8,8 @@ remote_file download_dest do
   action :create_if_missing
 end
 
-execute 'extract_wkhtmltopdf' do
+execute 'install wkhtmltopdf' do
   cwd cache_dir
-  command "tar -xjf #{download_dest}"
-  creates File.join(cache_dir, 'wkhtmltox')
-end
-
-execute 'install_wkhtmltoimage' do
-  cwd cache_dir
-  command "cp wkhtmltox/bin/wkhtmltoimage #{node['wkhtmltopdf']['install_dir']}/wkhtmltoimage"
-  creates "#{node['wkhtmltopdf']['install_dir']}/wkhtmltoimage"
-end
-
-execute 'install_wkhtmltopdf' do
-  cwd cache_dir
-  command "cp wkhtmltox/bin/wkhtmltopdf #{node['wkhtmltopdf']['install_dir']}/wkhtmltopdf"
-  creates "#{node['wkhtmltopdf']['install_dir']}/wkhtmltopdf"
-end
-
-unless node['wkhtmltopdf']['lib_dir'].empty?
-  execute 'install_wkhtmltox_so' do
-    cwd cache_dir
-    command "cp wkhtmltox/lib/libwkhtmltox.so.#{node['wkhtmltopdf']['version']} #{node['wkhtmltopdf']['lib_dir']}/libwkhtmltox.so.#{node['wkhtmltopdf']['version']}"
-    creates "#{node['wkhtmltopdf']['lib_dir']}/libwkhtmltox.so.#{node['wkhtmltopdf']['version']}"
-  end
-
-  link "#{node['wkhtmltopdf']['lib_dir']}/libwkhtmltox.so.#{wkhtmltopdf_version.major}.#{wkhtmltopdf_version.minor}" do
-    to "#{node['wkhtmltopdf']['lib_dir']}/libwkhtmltox.so.#{node['wkhtmltopdf']['version']}"
-  end
-
-  link "#{node['wkhtmltopdf']['lib_dir']}/libwkhtmltox.so.#{wkhtmltopdf_version.major}" do
-    to "#{node['wkhtmltopdf']['lib_dir']}/libwkhtmltox.so.#{node['wkhtmltopdf']['version']}"
-  end
+  command "dpkg -i #{download_dest}"
+  not_if "/usr/local/bin/wkhtmltopdf --version | grep #{wkhtmltopdf_version}"
 end
