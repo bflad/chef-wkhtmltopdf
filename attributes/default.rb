04d06f222e21298aeacd1635664e620608079bf6
Ian Ehlert <ehlertij@gmail.com>
2014-08-26 11:32:32 -0500

    put package dependencies back in

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,27 @@
 default['wkhtmltopdf']['version']     = '0.12.1'
 
+case node['platform_family']
+when 'mac_os_x', 'mac_os_x_server'
+  default['wkhtmltopdf']['dependency_packages'] = []
+  default['wkhtmltopdf']['platform'] = 'macosx-10.9.1-x86_64'
+when 'windows'
+  default['wkhtmltopdf']['dependency_packages'] = []
+  if node['kernel']['machine'] == 'x86_64'
+    default['wkhtmltopdf']['platform'] = 'win64'
+  else
+    default['wkhtmltopdf']['platform'] = 'win32'
+  end
+else
+  default['wkhtmltopdf']['dependency_packages'] = value_for_platform_family(
+    %w(debian) => %w(libfontconfig1 libssl0.9.8 libxext6 libxrender1),
+    %w(fedora rhel) => %w(fontconfig libXext libXrender openssl-devel urw-fonts)
+  )
+  if node['kernel']['machine'] == 'x86_64'
+    default['wkhtmltopdf']['platform'] = 'linux-amd64'
+  else
+    default['wkhtmltopdf']['platform'] = 'linux-i386'
+  end
+end
+
 default['wkhtmltopdf']['package']     = "wkhtmltox-#{node['wkhtmltopdf']['version']}_linux-precise-amd64.deb.deb"
 default['wkhtmltopdf']['mirror_url']  = "http://downloads.sourceforge.net/project/wkhtmltopdf/#{node['wkhtmltopdf']['version']}/#{node['wkhtmltopdf']['package']}"
