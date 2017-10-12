358018183d8820421fd22ad45490fa6d27cd3450
James Mac William <jimmy.macwilliam@gmail.com>
2017-10-12 09:44:58 -0400

    Add ubuntu 16.04 to vms for testing and update dependencies
    - updated dependencies to match list at:
    https://wkhtmltopdf.org/downloads.html
    
    - added ubuntu 16.04 and ran recipe to ensure that wkhtmltopdf binary
    installs successfully

--- attributes/default.rb
+++ attributes/default.rb
@@ -15,7 +15,7 @@ when 'windows'
   end
 else
   default['wkhtmltopdf']['dependency_packages'] = value_for_platform_family(
-    %w(debian) => %w(libfontconfig1 libssl0.9.8 libxext6 libxrender1),
+    %w(debian) => %w(zlib1g-dev libfontconfig1 libfreetype6-dev libx11-dev libxext6 libxrender1),
     %w(fedora rhel) => %w(fontconfig libXext libXrender openssl-devel urw-fonts)
   )
   if node['kernel']['machine'] == 'x86_64'
@@ -25,7 +25,5 @@ else
   end
 end
 
-#default['wkhtmltopdf']['archive']     = "wkhtmltox-#{node['wkhtmltopdf']['platform']}_#{node['wkhtmltopdf']['version']}-#{node['wkhtmltopdf']['build_sha']}.tar.xz"
 default['wkhtmltopdf']['archive']     = "wkhtmltox-#{node['wkhtmltopdf']['version']}_#{node['wkhtmltopdf']['platform']}"
-#default['wkhtmltopdf']['mirror_url']  = "http://downloads.sourceforge.net/project/wkhtmltopdf/#{node['wkhtmltopdf']['version']}/#{node['wkhtmltopdf']['archive']}"
 default['wkhtmltopdf']['mirror_url']  = "https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/#{node['wkhtmltopdf']['version']}/#{node['wkhtmltopdf']['archive']}"
