c0851a1ea58a024358a402eab53bf47d114ab529
James Mac William <jimmy.macwilliam@gmail.com>
2017-10-12 14:23:49 -0400

    Rubocop fixes

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,21 +8,21 @@ when 'mac_os_x', 'mac_os_x_server'
   default['wkhtmltopdf']['platform'] = 'osx-cocoa-x86-64.pkg'
 when 'windows'
   default['wkhtmltopdf']['dependency_packages'] = []
-  if node['kernel']['machine'] == 'x86_64'
-    default['wkhtmltopdf']['platform'] = 'msvc2015-win64.exe'
-  else
-    default['wkhtmltopdf']['platform'] = 'msvc2015-win32.exe'
-  end
+  default['wkhtmltopdf']['platform'] = if node['kernel']['machine'] == 'x86_64'
+                                         'msvc2015-win64.exe'
+                                       else
+                                         'msvc2015-win32.exe'
+                                       end
 else
   default['wkhtmltopdf']['dependency_packages'] = value_for_platform_family(
-    %w(debian) => %w(zlib1g-dev libfontconfig1 libfreetype6-dev libx11-dev libxext6 libxrender1),
-    %w(fedora rhel) => %w(fontconfig libXext libXrender openssl-devel urw-fonts)
+    %w[debian] => %w[zlib1g-dev libfontconfig1 libfreetype6-dev libx11-dev libxext6 libxrender1],
+    %w[fedora rhel] => %w[fontconfig libXext libXrender openssl-devel urw-fonts]
   )
-  if node['kernel']['machine'] == 'x86_64'
-    default['wkhtmltopdf']['platform'] = 'linux-generic-amd64.tar.xz'
-  else
-    default['wkhtmltopdf']['platform'] = 'linux-generic-i386.tar.xz'
-  end
+  default['wkhtmltopdf']['platform'] = if node['kernel']['machine'] == 'x86_64'
+                                         'linux-generic-amd64.tar.xz'
+                                       else
+                                         'linux-generic-i386.tar.xz'
+                                       end
 end
 
 default['wkhtmltopdf']['archive']     = "wkhtmltox-#{node['wkhtmltopdf']['version']}_#{node['wkhtmltopdf']['platform']}"
